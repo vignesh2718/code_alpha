3527366c360e5b9d711cd0385e2bfd15683de367
VIGNESH N <vigneshn2718@gmail.com>
2023-06-08 15:38:44 +0530

    updated

--- background_generator/app.js
+++ background_generator/app.js
@@ -1,9 +1,9 @@
 function changeMe(){
     var symbols,color;
-    symbols="012345789ABC";
+    symbols="012345789ABCDEF";
     color="#";
     for(var i=0;i<6;i++){
-        color=color+symbols[Math.floor(Math.random()*10)]; 
+        color=color+symbols[Math.floor(Math.random()*14)]; 
     }
     document.body.style.backgroundColor = color;
     document.getElementById("code").innerHTML = color;
